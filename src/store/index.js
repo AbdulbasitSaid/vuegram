14009fd5d0f1785420faea0a71134ca70e6b0266
abdulbasit <mrspeed518@gmail.com>
2020-11-07 06:11:14 +0100

    login and sign updates

--- src/store/index.js
+++ src/store/index.js
@@ -18,10 +18,27 @@ export default new Vuex.Store({
     async login({ dispatch }, form) {
       //sign in user
       const { user } = await auth.signInWithEmailAndPassword(form.email, form.password)
+      console.log(user);
+
       //fetch user profile and set state
       dispatch('fetchUserProfile', user)
     },
+    async signup({ dispatch }, form) {
+      // sign user up
+      const { user } = await auth.createUserWithEmailAndPassword(form.email, form.password)
+
+      // create user profile object in userCollections
+      await usersCollection.doc(user.uid).set({
+        name: form.name,
+        title: form.title
+      })
+
+      // fetch user profile and set in state
+      dispatch('fetchUserProfile', user)
+    },
     async fetchUserProfile({ commit }, user) {
+      console.log(user);
+
       // fetch user profile
       const userProfile = await usersCollection.doc(user.id).get()
       //set user profile to state
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,8 +9,8 @@
           powered by Vue.js and Firebase.
         </p>
       </div>
-      <div class="col2">
-        <form @submit.prevent>
+      <div :class="{ 'signup-form': !showLoginForm }" class="col2">
+        <form v-if="showLoginForm" @submit.prevent>
           <h1>Welcome Back</h1>
           <div>
             <label for="email1">Email</label>
@@ -33,7 +33,50 @@
           <button @click="login()" class="button">Log In</button>
           <div class="extras">
             <a>Forgot Password</a>
-            <a>Create an Account</a>
+            <a @click="toggleForm()">Create an Account</a>
+          </div>
+        </form>
+        <form v-else @submit.prevent>
+          <h1>Get Started</h1>
+          <div>
+            <label for="name">Name</label>
+            <input
+              v-model.trim="signupForm.name"
+              type="text"
+              placeholder="Savvy Apps"
+              id="name"
+            />
+          </div>
+          <div>
+            <label for="title">Title</label>
+            <input
+              v-model.trim="signupForm.title"
+              type="text"
+              placeholder="Company"
+              id="title"
+            />
+          </div>
+          <div>
+            <label for="email2">Email</label>
+            <input
+              v-model.trim="signupForm.email"
+              type="text"
+              placeholder="you@email.com"
+              id="email2"
+            />
+          </div>
+          <div>
+            <label for="password2">Password</label>
+            <input
+              v-model.trim="signupForm.password"
+              type="password"
+              placeholder="min 6 characters"
+              id="password2"
+            />
+          </div>
+          <button @click="signup()" class="button">Sign Up</button>
+          <div class="extras">
+            <a @click="toggleForm()">Back to Log In</a>
           </div>
         </form>
       </div>
@@ -50,9 +93,27 @@ export default {
         password: this.loginForm.password,
       });
     },
+    signup() {
+      this.$store.dispatch("signup", {
+        email: this.signupForm.email,
+        password: this.signupForm.password,
+        name: this.signupForm.name,
+        title: this.signupForm.title,
+      });
+    },
   },
   data() {
     return {
+      showLoginForm: true,
+      toggleForm() {
+        this.showLoginForm = !this.showLoginForm;
+      },
+      signupForm: {
+        name: "",
+        title: "",
+        email: "",
+        password: "",
+      },
       loginForm: {
         email: "",
         password: "",
