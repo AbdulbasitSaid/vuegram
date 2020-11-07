16fd031610da84f0ebc22e044a49a4c25acce0b9
abdulbasit <mrspeed518@gmail.com>
2020-11-07 03:25:48 +0100

    login ui

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,39 @@
 <template>
-    <div>
-        Login
-    </div>
+  <div id="login">
+    <section>
+      <div class="col1">
+        <h1>Vuegram</h1>
+        <p>
+          Welcome to the
+          <a href="#" target="_blank">VueGram</a> sample social media web app
+          powered by Vue.js and Firebase.
+        </p>
+      </div>
+      <div class="col2">
+        <form>
+          <h1>Welcome Back</h1>
+          <div>
+            <label for="email1">Email</label>
+            <input type="text" placeholder="you@email.com" id="email1" />
+          </div>
+          <div>
+            <label for="password1">Password</label>
+            <input type="password" placeholder="******" id="password1" />
+          </div>
+          <button class="button">Log In</button>
+          <div class="extras">
+            <a>Forgot Password</a>
+            <a>Create an Account</a>
+          </div>
+        </form>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
